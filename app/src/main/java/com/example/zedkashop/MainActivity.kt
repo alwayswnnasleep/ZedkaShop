a38944e683173813e6754c77374055640a1c942e
Pavel <alwayswnnasleep@gmail.com>
2024-09-28 19:33:50 +0300

    Убрана ActionBar

--- app/src/main/java/com/example/zedkashop/MainActivity.kt
+++ app/src/main/java/com/example/zedkashop/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.zedkashop
 
 import android.os.Bundle
-
 import androidx.appcompat.app.AppCompatActivity
-
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.zedkashop.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -16,26 +13,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_ZedkaShop)
         super.onCreate(savedInstanceState)
 
 
+        supportActionBar?.show()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
 
-        // Get the NavHostFragment and its NavController
+        // Получаем NavHostFragment и его NavController
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_cart, R.id.navigation_favorites, R.id.navigation_profile
-            )
-        )
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        // Убираем настройку Action Bar с NavController
         navView.setupWithNavController(navController)
     }
 }
