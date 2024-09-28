ad03254931ca507d7b50084df60b00e12b468ca3
Pavel <alwayswnnasleep@gmail.com>
2024-09-28 10:11:53 +0300

    Добавлена BottomNavigationBar

--- app/src/main/java/com/example/zedkashop/MainActivity.kt
+++ app/src/main/java/com/example/zedkashop/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.zedkashop
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.zedkashop.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,13 +21,16 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_view)
+        // Get the NavHostFragment and its NavController
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_cart, R.id.navigation_favorites,R.id.navigation_profile
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_cart, R.id.navigation_favorites, R.id.navigation_profile
             )
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
